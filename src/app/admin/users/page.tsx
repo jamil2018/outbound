f441912093443f418c55dc4573023f103ac61e68
jamil2018 <hasnatjamil2018@gmail.com>
2023-09-11 01:17:43 +0600

    changeset: update debug changes

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,22 +1,22 @@
-// import { User } from "@/models/UserModel";
-// import { DataTable } from "../../../components/ui/data-table";
-// import { columns } from "./columns";
+import { User } from "@/models/UserModel";
+import { DataTable } from "../../../components/ui/data-table";
+import { columns } from "./columns";
 
 export default async function Users() {
-  // async function getUsers(): Promise<User[]> {
-  //   const res = await fetch(`${process.env.BASE_URL}/api/users`);
-  //   if (!res.ok) {
-  //     console.log(res);
-  //   }
-  //   return res.json();
-  // }
-  // const data = await getUsers();
+  async function getUsers(): Promise<User[]> {
+    const res = await fetch(`${process.env.BASE_URL}/api/users`);
+    if (!res.ok) {
+      console.log(res);
+    }
+    return res.json();
+  }
+  const data = await getUsers();
   return (
     <>
       <h1>All users</h1>
-      {/* <div className="container mx-auto py-10">
+      <div className="container mx-auto py-10">
         <DataTable columns={columns} data={data} />
-      </div> */}
+      </div>
     </>
   );
 }
